aaae01453ce684bc53c7b933d6c990ceffd3aa6c
Faustvii <Faustvii@users.noreply.github.com>
2025-07-12 23:34:48 +0200

    fix: ensure book file count is returned in output

--- src/NzbDrone.Integration.Test/ApiTests/BookApiFixture.cs
+++ src/NzbDrone.Integration.Test/ApiTests/BookApiFixture.cs
@@ -2,24 +2,42 @@ using System;
 using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
+using NzbDrone.Core.Books;
+using NzbDrone.Core.Qualities;
+using Readarr.Api.V1.RootFolders;
 
 namespace NzbDrone.Integration.Test.ApiTests
 {
     [TestFixture]
     public class BookApiFixture : IntegrationTest
     {
+        [SetUp]
+        public void Setup()
+        {
+            RootFolders.Post(new RootFolderResource
+            {
+                Name = "TestLibrary",
+                Path = AuthorRootFolder,
+                DefaultMetadataProfileId = 1,
+                DefaultQualityProfileId = 1,
+                DefaultMonitorOption = MonitorTypes.All
+            });
+        }
+
         [Test]
         public void get_books_fallback_should_return_books()
         {
             // Ensure at least one author and book exist
-            var author = EnsureAuthor("3910549", "179778914", "Actus", true);
+            var foreignEditionId = "179778914";
+            var author = EnsureAuthor("3910549", foreignEditionId, "Actus", true);
+            EnsureBookFile(author, 1, foreignEditionId, Quality.MOBI);
 
             // Call the /api/v1/book endpoint with no filters (fallback path)
             var books = Books.All();
 
             // Assert that the response contains at least one book for the author
             books.Should().NotBeNullOrEmpty();
-            var book = books.FirstOrDefault(b => b.AuthorId == author.Id);
+            var book = books.FirstOrDefault(b => b.AuthorId == author.Id && b.ForeignEditionId == foreignEditionId);
             book.Should().NotBeNull();
             book.Title.Should().NotBeNullOrWhiteSpace();
             book.AuthorTitle.Should().NotBeNullOrWhiteSpace();
@@ -41,11 +59,11 @@ namespace NzbDrone.Integration.Test.ApiTests
             book.Images.Should().NotBeNull();
             book.Links.Should().NotBeNull();
             book.Statistics.Should().NotBeNull();
-            book.Statistics.BookFileCount.Should().BeGreaterOrEqualTo(0);
-            book.Statistics.BookCount.Should().BeGreaterOrEqualTo(0);
-            book.Statistics.TotalBookCount.Should().BeGreaterOrEqualTo(0);
-            book.Statistics.SizeOnDisk.Should().BeGreaterOrEqualTo(0);
-            book.Statistics.PercentOfBooks.Should().BeGreaterOrEqualTo(0);
+            book.Statistics.BookFileCount.Should().BeGreaterThan(0);
+            book.Statistics.BookCount.Should().BeGreaterThan(0);
+            book.Statistics.TotalBookCount.Should().BeGreaterThan(0);
+            book.Statistics.SizeOnDisk.Should().BeGreaterThan(0);
+            book.Statistics.PercentOfBooks.Should().BeGreaterThan(0);
             book.Added.Should().NotBe(default(DateTime));
             book.Grabbed.Should().BeFalse();
             book.Id.Should().BeGreaterThan(0);
